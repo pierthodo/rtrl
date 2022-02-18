6239709c0bface0a38a57a033306ebda6ae192d0
Pierre Thodoroff <pierthodo@gmail.com>
2022-02-18 09:50:55 +0000

    Fix checkpoint path

--- rtrl/__init__.py
+++ rtrl/__init__.py
@@ -28,8 +28,9 @@ def iterate_episodes(run_cls: type = Training, checkpoint_path: str = None):
   """
   checkpoint_path = checkpoint_path or tempfile.mktemp("_remove_on_exit")
 
-  checkpoint_path = "./resources/checkpoint/" + datetime.now().strftime("%m:%d:%Y-%H:%M:%S")
+  checkpoint_path = "./resources/checkpoint/" + datetime.now().strftime("%m-%d-%Y-%H-%M-%S")
   os.mkdir(checkpoint_path)
+  checkpoint_path += "store"
   try:
     if not exists(checkpoint_path):
       print("=== specification ".ljust(70, "="))
